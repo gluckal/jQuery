21a729280e29ef6167eb7955974f50f37cae038b
Alexandra Gluck <40146635+gluckal@users.noreply.github.com>
2019-12-04 16:40:22 -0500

    Delete challenge3.js

--- jQuery_homework/js/challenge3.js
+++
@@ -1,9 +0,0 @@
-$('form').submit(function(onsubmit){
-    var fruit = $('input[name="fruit"]');
-    var standing = $('input[name="standing"]');
-    if ((fruit.is(":checked")) && (standing.is(":checked"))) {
-        return true;
-    }
-    alert("You must pick a fruit and a year!");
-    return false
-});
